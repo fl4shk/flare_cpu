2563fa83788812cdd28be3463c0d0f4ede88b723
fl4shk <fl4shk@users.noreply.github.com>
2023-04-17 08:47:26 -0500

    update test_0

--- test_asm/test_0/test_0.s
+++ test_asm/test_0/test_0.s
@@ -3,50 +3,58 @@
 //.org 0x1000
 .global _start
 _start:
-	//cpy r0, #0x3
-	//add sp, #0x3
-	//cpy r0, #_start - test_label
-	//bra #_start
-	//bra #test_label
-	//bra #test_label_2
-	//bra #branch_target
 	//cpy r2, fp
 branch_target:
-	//bra #0x80000000
-	bgtu #branch_target
-	bra #test_label_2
-	//cpy r0, #test_label_2
-	//cpy r3, #0x1ff
-	//cpy r3, #_start
-	//cpy sp, #_start
-	//cpy r0, r1
-	//bra #_start
+	//bra 0x80000000
+	//bgtu branch_target
+	//bra test_label_2
+	cpy r1, r3
 test_label:
-	//bra #test_label_2 - _start
+	//bra test_label_2 - _start
 	//cpy fp, #test_label_2 - _start
-	//#cpy fp, #branch_target - test_label 
-	//bra #test_label
-	//bra #test_label
-	//bra #_start
+	//cpy fp, #branch_target - test_label 
 
 //.org 0x1000
 test_label_2:
-	//bra #_start
-	//bra #test_label_2
+	//bra _start
+	//bra test_label_2
 	//cpy r2, sp
 
 //.org 0x2000
 test_label_3:
-	cpy sp, #test_label_3 - test_label_2
-	//cpy 
+	//bra branch_target
+	//cpy sp, #test_label_3 - test_label_2
+	//cpy sp, #test_label_3 - _start
+	//cpy sp, #_start
+	//cpy fp, #3
+	//cpy fp, #0x3333
+	//add r0, #0x3
+	//add r0, pc, #3
+	//sub r3, fp
+	//cmp sp, fp
+	cpy hi, fp
+	cpy fp, hi
+	reti
+	jmp ira
+	push r0, sp
+	pop hi, sp
+	push hi, sp
+	//.word 0x10000000
+	//.word test_label_3
+	cpy r4, #3
+	jmp r5
+	//ldr r0, [r5, #0x0]
+	//str r0, [r5, #0x3]
+	//bra 0x2
 	//cpy fp, #test_label_2 - _start
-	//bra #test_label_3 - test_label_2
-	bra #test_label_2
-	cpy r0, #3
+	//bra test_label_3 - test_label_2
+	//bra test_label_2
+	//cpy r0, #3
 
 .data
 //.org 0x1008
 test_data:
+	.word _start
 	//.byte 0x88, 0x89
 	//.word test_label_3
-	.word test_data
+	//.word test_data
