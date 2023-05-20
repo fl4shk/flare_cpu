f228af57baf8c24dea04db39f7eea6bf54c519b1
fl4shk <fl4shk@users.noreply.github.com>
2023-05-20 10:11:58 -0500

    ISA spec: run `make` to generate a new PDF `test_code/bra_dot_nr_test_0`: add more tests for Binutils (got stuff fixed!)

--- test_code/bra_dot_nr_test_0/src/bra_dot_nr_test_0.s
+++ test_code/bra_dot_nr_test_0/src/bra_dot_nr_test_0.s
@@ -2,11 +2,51 @@
 .text
 .global _start
 _start:
-	//bra.nr _start
-	cpy.nr r0, #3
+	////cpy r0, #0xffff
+	////cpy r1, #testificate
+	////cpy r2, #asdf
+	////lsl r3, #5
+	////cpy r2, r3
+	////lsl r3, #5
+	////cpy r4, r5
+	//cpy r3, r5
+	//bra _start
+	//bra target
+	//cpy r2, r3
+	//cpy r3, r5
+	////cpy r2, r3
 
-test:
-	bra.nr test
+	//bra testificate
+	bra target
 
-testificate:
+//.org 0x200034
+.org 0x32000
+target:
+	//cpy r0, #target
+	//cpy r0, #0x3333
+	//cpy r0, #_start
+	cpy r0, #target
+	lsr r0, #target + 0x3000
+	bra.nr target
+	bra _start
 	bra testificate
+//infin:
+//	bra infin
+//	bra testificate
+
+//some_data:
+//	.hword some_data
+//	.hword target
+//	.hword some_data - infin 
+
+asdf = 0x3333
+//	//bra.nr _start
+//	cpy.nr r0, #3
+//
+//test:
+//	bra.nr test
+//
+//testificate:
+//	bra testificate
+//	//jmp 0
+//	//ret
---
+++ test_code/bra_dot_nr_test_0/src/testificate.s
@@ -0,0 +1,8 @@
+.align 2
+.text
+.global testificate
+.org 0xdffe
+testificate:
+	cpy r0, #_start
+	//bra 0xffffff
+	//bra _start
